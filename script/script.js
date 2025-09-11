b1ca624b0ec5439d27a68913ecd0c3e26d19c4ae
diogomartins <diogocmartins77@gmail.com>
2025-09-11 00:27:48 -0300

    iniciando javascrit

---
+++ script/script.js
@@ -0,0 +1,66 @@
+const produtosPadaria = {
+    bolosPadaria : {
+        tradicionais: ['Brigadeiro', 'Casadinho', 'Prestigio'],
+        frutas: ['Creme com Morango', 'Creme com Abacaxi'],
+        Gurmet: ['Trufado']
+    },
+
+    docinhosFesta : {
+        tradicionais: ['Brigadeiro', 'Casadinho', 'beijinho'],
+        Gurmet: ['ninho com nuttela', 'olho de sogra', 'bicho de pé']
+    },
+
+    salgadoPadaria : {
+        frito: {
+            mini: {
+                coxinha: ['Frango', 'Carne'],
+                risoles: 'Presunto e queijo'
+            },
+            grande: {
+                coxinha: ['Frango', 'Carne'],
+                risoles: 'Presunto e queijo',
+            }
+        },
+
+        assado: {
+            mini: {
+                enroladinhos: ['frios', 'salsisha'],
+                croissant: ['carne', 'Frango']
+            },
+            grande: {
+                hambuguer: ['cheddar e bacon', 'frango catupiry'],
+                salsicha: ['cheddar e bacon', 'frango catupiry'],
+                tortas: ['frango']
+            }
+        }
+    },
+
+    paosPadaria: {
+        paes: {
+            tradicionais: ['italiano', 'Frances', 'australiano', 'da Casa', 'torradas'],
+            doce: ['rosca', 'lingua de sogra'],
+            recheado: ['presunto e queijo', 'peito de peru']
+
+        }
+    },
+
+    docesPadria : {
+        copo: ['mousse de maracuja'],
+        torta: ['limão'],
+        bolachinha: ['sequilhos'],
+        vitrine: ['croissant de chocolate', 'bombas', 'carolinas']
+    }
+}
+
+
+//exibindo produto ao pesquisar
+function Pesquisar(){
+      let pesquisar = document.getElementById(valorPesquisa)
+    
+    
+    }
+
+    
+
+
+
